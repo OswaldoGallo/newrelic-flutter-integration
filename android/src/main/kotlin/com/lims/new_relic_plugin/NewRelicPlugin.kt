19b935a66bb7ce8d026ea5474ed66f6dbc252485
Oswaldo Gallo <oswaldo.gallor@gmail.com>
2020-05-27 18:13:51 -0500

    Adds NewRelic custom data types methods

--- android/src/main/kotlin/com/lims/new_relic_plugin/NewRelicPlugin.kt
+++ android/src/main/kotlin/com/lims/new_relic_plugin/NewRelicPlugin.kt
@@ -11,72 +11,115 @@ import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** NewRelicPlugin */
-public class NewRelicPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+public class NewRelicPlugin : FlutterPlugin, MethodCallHandler {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private lateinit var channel: MethodChannel
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.flutterEngine.dartExecutor, "new_relic_plugin")
-    channel.setMethodCallHandler(this);
-  }
-
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  companion object {
-    private const val SUCCESS = "Success: The provided attribute was successfully submitted to New Relic -> "
-    private const val ERROR = "Error: Cannot submit the provided attribute -> "
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "new_relic_plugin")
-      channel.setMethodCallHandler(NewRelicPlugin())
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.flutterEngine.dartExecutor, "new_relic_plugin")
+        channel.setMethodCallHandler(this);
     }
-  }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    when (call.method) {
-        "getPlatformVersion" -> {
-          result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    // This static function is optional and equivalent to onAttachedToEngine. It supports the old
+    // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
+    // plugin registration via this function while apps migrate to use the new Android APIs
+    // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
+    //
+    // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
+    // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
+    // depending on the user's project. onAttachedToEngine or registerWith must both be defined
+    // in the same class.
+    companion object {
+        @JvmStatic
+        fun registerWith(registrar: Registrar) {
+            val channel = MethodChannel(registrar.messenger(), "new_relic_plugin")
+            channel.setMethodCallHandler(NewRelicPlugin())
         }
-        "setStringAttribute" -> {
-          val name: String = call.argument("name")!!
-          val value: String? = call.argument("value")
-          if (NewRelic.setAttribute(name, value)) {
-            result.success("$SUCCESS $name: $value")
-          } else {
-            result.error("$ERROR $name: $value", null, null)
-          }
-
-        }
-        else -> result.notImplemented()
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        when (call.method) {
 
-  //  Add session-level custom attributes
+            "getPlatformVersion" -> {
+                result.success("Android ${android.os.Build.VERSION.RELEASE}")
+            }
 
-  private fun setAttribute(name: String, value: String) {
-    NewRelic.setAttribute(name, value)
-  }
+            // Add session-level custom attributes
 
-  private fun setAttribute(name: String, value: Double) {
-    NewRelic.setAttribute(name, value)
-  }
+            "setStringAttribute" -> {
+                val name: String = call.argument("name")!!
+                val value: String = call.argument("value")!!
+                val bool = NewRelic.setAttribute(name, value)
+                result.success(bool)
+            }
 
-//  fun incrementAttribute(name: String, value: Float? = null, boolean: Boolean? = null) {
-//    NewRelic.incrementAttribute(name, value)
-//  }
+            "setDoubleAttribute" -> {
+                val name: String = call.argument("name")!!
+                val value: Double = call.argument("value")!!
+                val bool = NewRelic.setAttribute(name, value)
+                result.success(bool)
+            }
+
+            "setBooleanAttribute" -> {
+                val name: String = call.argument("name")!!
+                val value: Boolean = call.argument("value")!!
+                val bool = NewRelic.setAttribute(name, value)
+                result.success(bool)
+            }
+
+            "incrementAttribute" -> {
+                val name: String = call.argument("name")!!
+                val value: Double = call.argument("value")!!
+                val bool = NewRelic.incrementAttribute(name, value)
+                result.success(bool)
+            }
+
+            // Record breadcrumbs
+
+            "recordBreadcrumb" -> {
+                val breadcrumbName: String = call.argument("breadcrumbName")!!
+                val attributes: Map<String, Any>? = call.argument("attributes")
+                val bool = NewRelic.recordBreadcrumb(breadcrumbName, attributes)
+                result.success(bool)
+            }
+
+            // Create custom interactions
+
+            "startInteraction" -> {
+                val actionName: String = call.argument("actionName")!!
+                val id : String? = NewRelic.startInteraction(actionName)
+                if (!id.isNullOrEmpty()) {
+                    result.success(id)
+                } else {
+                    result.error("Error!", null, null)
+                }
+            }
+
+            "endInteraction" -> {
+                val id: String = call.argument("id")!!
+                NewRelic.endInteraction(id)
+                result.success("Interaction $id ended")
+            }
+
+            // Record custom events
+
+            "recordCustomEvent" -> {
+                val eventType: String = call.argument("eventType")!!
+                val eventName: String? = call.argument("eventName")!!
+                val eventAttributes: Map<String, Any>? = call.argument("eventAttributes")!!
+                val bool = NewRelic.recordCustomEvent(eventType, eventName, eventAttributes)
+                result.success(bool)
+            }
+
+            else -> result.notImplemented()
+        }
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 
 }
--- lib/new_relic_plugin.dart
+++ lib/new_relic_plugin.dart
@@ -6,17 +6,72 @@ class NewRelicPlugin {
   static const MethodChannel _channel =
       const MethodChannel('new_relic_plugin');
 
-  static Future<String> setStringAttribute(String name, dynamic value) async {
+  static Future<bool> setStringAttribute(String name, String value) async {
+    final Map<String, dynamic> params = <String, String> {
+      'name': name,
+      'value': value
+    };
+    final bool result = await _channel.invokeMethod('setStringAttribute', params);
+    return result;
+  }
 
+  static Future<bool> setDoubleAttribute(String name, double value) async {
     final Map<String, dynamic> params = <String, dynamic> {
       'name': name,
       'value': value
     };
-
-    final String result = await _channel.invokeMethod('setStringAttribute', params);
-
+    final bool result = await _channel.invokeMethod('setDoubleAttribute', params);
     return result;
+  }
 
+  static Future<bool> setBooleanAttribute(String name, bool value) async {
+    final Map<String, dynamic> params = <String, dynamic> {
+      'name': name,
+      'value': value
+    };
+    final bool result = await _channel.invokeMethod('setBooleanAttribute', params);
+    return result;
+  }
+
+  static Future<bool> incrementAttribute(String name, {double value = 1.0}) async {
+    final Map<String, dynamic> params = <String, dynamic> {
+      'name': name,
+      'value': value
+    };
+    final bool result = await _channel.invokeMethod('incrementAttribute', params);
+    return result;
+  }
+
+  static Future<bool> recordBreadcrumb(String breadcrumbName, { Map<String, dynamic> attributes} ) async {
+    final Map<String, dynamic> params = <String, dynamic> {
+      'breadcrumbName': breadcrumbName,
+      'attributes': attributes
+    };
+    final bool result = await _channel.invokeMethod('recordBreadcrumb', params);
+    return result;
+  }
+
+  static Future<String> startInteraction(String actionName) async {
+    final Map<String, String> params = <String, String> {
+      'actionName': actionName
+    };
+    final String result = await _channel.invokeMethod('startInteraction', params);
+    return result;
+  }
+
+  static void endInteraction(String id) async {
+    final Map<String, String> params = <String, String> { 'id': id };
+    print(await _channel.invokeMethod('endInteraction', params));
+  }
+
+  static Future<bool> recordCustomEvent(String eventType, {String eventName = "", Map<String, dynamic> eventAttributes} ) async {
+    final Map<String, dynamic> params = <String, dynamic> {
+      'eventType': eventType,
+      'eventName': eventName,
+      'eventAttributes': eventAttributes
+    };
+    final bool result = await _channel.invokeMethod('recordCustomEvent', params);
+    return result;
   }
 
   static Future<String> get platformVersion async {
